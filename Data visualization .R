1695dab4003122fadcdadb57b9c5394c79eca289
NanjunZhu <101536036+NanjunZhu@users.noreply.github.com>
2022-03-15 11:14:28 +0000

    chagen table on Readme file

--- Data visualization .R
+++ Data visualization .R
@@ -2,8 +2,14 @@
 library(tidyverse)
 # import dataset and remove NA from dataset. 
 data.origin <- read_csv("dataset8.csv")
-data.no.NA <- data.originn %>% 
+data.no.NA <- data.origin %>% 
   na.omit()
 
 glimpse(data.no.NA)
-## 
+## Qualityclass as our response variables,, 
+
+
+
+
+
+
