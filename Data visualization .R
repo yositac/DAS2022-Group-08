5f6b3a25388eee3a384a5270a989aea74e88a016
yositac <99663473+yositac@users.noreply.github.com>
2022-03-18 00:12:54 +0000

    Delete Data visualization .R

--- Data visualization .R
+++
@@ -1,9 +0,0 @@
-##load library 
-library(tidyverse)
-# import dataset and remove NA from dataset. 
-data.origin <- read_csv("dataset8.csv")
-data.no.NA <- data.originn %>% 
-  na.omit()
-
-glimpse(data.no.NA)
-## 
